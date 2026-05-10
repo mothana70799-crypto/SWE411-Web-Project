b91f666f49ca2f2273ffc0b6cc84b6a3ba94e95f
Mothana Alhutheel <mothana70799@gmail.com>
2026-05-10 20:35:12 +0300

    مهممممم
    صار عندي تعارض بالملف بالرفع برفعه مره ثانيه واذا صار تكرار بشوفه

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
+
+namespace TodoApp.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<TodoItem> Todos => Set<TodoItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Username)
+            .IsUnique();
+
+        modelBuilder.Entity<TodoItem>()
+            .HasOne(t => t.User)
+            .WithMany(u => u.Todos)
+            .HasForeignKey(t => t.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
